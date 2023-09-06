d413135146d15cbe206688472f837eb3f8a55c9d
通晓宇宙 <2581011320@qq.com>
2023-09-07 01:19:38 +0800

    引入Swagger

--- payment-demo/src/main/java/com/atguigu/paymentdemo/controller/ProductController.java
+++ payment-demo/src/main/java/com/atguigu/paymentdemo/controller/ProductController.java
@@ -4,10 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(tags = "商品管理")
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
 
+    @ApiOperation("测试接口")
     @GetMapping("/test")
     public String test() {
 
