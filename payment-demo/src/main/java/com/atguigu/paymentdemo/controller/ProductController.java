f137a5aecbf65949a2d18d202ff2e1d32f30d2de
通晓宇宙 <2581011320@qq.com>
2023-09-07 01:13:43 +0800

    创建SpringBoot项目

---
+++ payment-demo/src/main/java/com/atguigu/paymentdemo/controller/ProductController.java
@@ -0,0 +1,17 @@
+package com.atguigu.paymentdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    @GetMapping("/test")
+    public String test() {
+
+        return "Hello";
+    }
+
+}
