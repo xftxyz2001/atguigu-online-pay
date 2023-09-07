f380886f6fb8a642a611330f9dffa3e71850ebdf
通晓宇宙 <2581011320@qq.com>
2023-09-07 16:43:17 +0800

    引入支付参数

---
+++ payment-demo/src/main/java/com/atguigu/paymentdemo/config/WxPayConfig.java
@@ -0,0 +1,38 @@
+package com.atguigu.paymentdemo.config;
+
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:wxpay.properties") //读取配置文件
+@ConfigurationProperties(prefix = "wxpay") //读取wxpay节点
+@Data //使用set方法将wxpay节点中的值填充到当前类的属性中
+@Slf4j
+public class WxPayConfig {
+
+    // 商户号
+    private String mchId;
+
+    // 商户API证书序列号
+    private String mchSerialNo;
+
+    // 商户私钥文件
+    private String privateKeyPath;
+
+    // APIv3密钥
+    private String apiV3Key;
+
+    // APPID
+    private String appid;
+
+    // 微信服务器地址
+    private String domain;
+
+    // 接收结果通知地址
+    private String notifyDomain;
+
+}
